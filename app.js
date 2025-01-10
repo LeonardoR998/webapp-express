2c1180544810ccdce48e1390a1142c13d1d60acc
LeonardoR998 <leonardoricci839@gmail.com>
2025-01-10 14:30:48 +0100

    add: env.example, folder routers and controllers

--- app.js
+++ app.js
@@ -3,8 +3,15 @@
 const express = require("express");
 const app = express();
 
+const { APP_HOST, APP_PORT } = process.env;
+
+//! ROUTES
+
+const movieRouter = require("./routers/movieRouter");
+app.use("/movie", movieRouter);
+
 //! SERVER LISTENING
 
 app.listen(3000, () => {
-  console.log("Server listening at http://localhost:3000");
+  console.log(`Server listening at ${APP_HOST}:${APP_PORT}`);
 });
