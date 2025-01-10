7335efc6cd1b166e2301ed4151742101a87df103
LeonardoR998 <leonardoricci839@gmail.com>
2025-01-10 15:10:44 +0100

    add middlewares

--- app.js
+++ app.js
@@ -5,10 +5,20 @@ const app = express();
 
 const { APP_HOST, APP_PORT } = process.env;
 
+//! MIDDLEWARES
+app.use(express.json());
+app.use(express.static("public"));
+
 //! ROUTES
 
 const movieRouter = require("./routers/movieRouter");
 app.use("/movie", movieRouter);
+
+//! ERROR HANDLERS
+const notFound = require("./middleware/notFound");
+const errorsHandler = require("./middleware/errorsHandler");
+app.use(errorsHandler);
+app.use(notFound);
 
 //! SERVER LISTENING
 
---
+++ middleware/notFound.js
@@ -0,0 +1,10 @@
+function notFound(req, res, next) {
+  res.status(404);
+  res,
+    json({
+      status: "KO",
+      error: "Page not found",
+    });
+}
+
+module.exports = notFound;
