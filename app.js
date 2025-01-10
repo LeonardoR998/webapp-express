744df7b4dfa9eeea2a9fd3f7bc4f5d6859ca9c0b
LeonardoR998 <leonardoricci839@gmail.com>
2025-01-10 14:03:48 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+//! CONFIG EXPRESS
+
+const express = require("express");
+const app = express();
+
+//! SERVER LISTENING
+
+app.listen(3000, () => {
+  console.log("Server listening at http://localhost:3000");
+});
